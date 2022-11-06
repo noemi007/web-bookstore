8de4709a25f4f9cc8093e59b86ae6dad50f187c4
Amtrix <naomi.yoriji@amtrix.jp>
2022-11-06 12:57:16 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+{
+
+
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const observer = new IntersectionObserver(callback, {
+    threshold:0.2,
+  });
+  
+  document.querySelectorAll('.animate').forEach(el => {
+    observer.observe(el);
+  });
+
+}
+
